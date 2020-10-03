5b3a3ec8813e0ab84277d83f4dbc83681b7d6670
Himanshoo Sharma <himanshoosharma@live.com>
2020-10-04 00:12:41 +0200

    Created the login after signup(created view currenttodos )

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -9,9 +11,18 @@ def signupuser(request):
         return render(request,'todo/signupuser.html',{'form':UserCreationForm()})
     else:
         # Create a new user
-        if request.POST['password1'] == request.POST['password2']
-            user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
-            user.save()
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+                user.save()
+                login(request,user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request,'todo/signupuser.html',{'form':UserCreationForm(),'error':'That username is already taken. Please choose a new username.'})
+
         else:
             # Tell the user passwords don't match
-            print('hello')
+            return render(request,'todo/signupuser.html',{'form':UserCreationForm(),'error':'Passwords do not match!'})
+
+def currenttodos(request):
+    return render(request,'todo/currenttodos.html')
